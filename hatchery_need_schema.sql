e69700593490d7bc3fbf5ae2ace50daaa04d5ef1
Adam Jensen <aejensen11@gmail.com>
2022-02-26 11:05:12 -0800

    push raw dam data

---
+++ hatchery_need_schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE projected_hatchery_need (
+	"Nwr_Population_Name" VARCHAR(500) NOT NULL,
+	"Number_Of_Spawners" FLOAT,
+	"Fracwild" DECIMAL,
+	"Effective_Catch" FLOAT
+	)
--- schema.sql
+++ schema.sql
@@ -1,10 +1,12 @@
 CREATE TABLE salmon_data (
-	ID INT NOT NULL,
-	"Brood Year" INT NOT NULL,
-	"Nwr Population Name" VARCHAR(500) NOT NULL,
-	"Number of Spawners" FLOAT,
-	"Start Year" FLOAT,
-	"End Year" FLOAT,
-	"Effective Catch" FLOAT,
-	"Fracwild" DECIMAL
+	"Brood_Year" INT NOT NULL,
+	"Nwr_Population_Name" VARCHAR(500) NOT NULL,
+	"Number_Of_Spawners" FLOAT,
+	"Start_Year" FLOAT,
+	"End_Year" FLOAT,
+	"Effective_Catch" FLOAT,
+	"Fracwild" DECIMAL,
+	"Spawners_Prev_Yr" FLOAT,
+	"Eff_Catch_Prev_Yr" FLOAT,
+	"Fracwild_Prev_Yr" FLOAT
 );
